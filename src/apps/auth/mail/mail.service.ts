1ca00e2eaf91ef7d280918110241fa9a1bce2bb5
abredagn <abredagn@gmail.com>
2025-08-04 07:14:00 +0300

    mailer logic

--- src/apps/auth/mail/mail.service.ts
+++ src/apps/auth/mail/mail.service.ts
@@ -20,7 +20,7 @@ export class MailService {
         const baseUrl = this.configService.get<string>('BASE_URL');
         const verificationUrl = `${baseUrl}/auth/verify-email?token=${token}`;
         const mailOptions = {
-            from: this.configService.get<string>('EMAIL_FROM'),
+            from: this.configService.get<string>('EMAIL_USER'),
             to: email,
             subject: 'Email Verification',
             text: `Please verify your email by clicking on the following link: ${verificationUrl}`,
