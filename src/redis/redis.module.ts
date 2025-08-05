65ac261b5c96e0ebc1f98829433c8e59a086dae3
abredagn <abredagn@gmail.com>
2025-08-05 21:22:25 +0300

    config way to access env applied instead of hardcoding

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import { RedisService } from './redis.service';
 import { redisStore } from 'cache-manager-ioredis-yet';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: async () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
         store: await redisStore({
-          host: 'localhost', 
-          port: 6379,        
-          ttl: 0,            
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+          password: configService.get<string>('REDIS_PASSWORD', ''),
+          ttl: configService.get<number>('REDIS_TTL', 0),
         }),
       }),
     }),
