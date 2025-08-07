abcea9cacbbeef033b3b2c914bcdde9014200cc4
abredagn <abredagn@gmail.com>
2025-08-07 12:23:35 +0300

    just find use by email

--- src/apps/users/users.service.ts
+++ src/apps/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +13,13 @@ export class UsersService {
 
     async updateLastLogin(id: string) {
         await this.usersRepository.update(id, {lastLoginAt: new Date()})
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await this.usersRepository.findOne({where: {email}})
+        if(!user) {
+            return null;
+        }
+        return user;
     }
 }
